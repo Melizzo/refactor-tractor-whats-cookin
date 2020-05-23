58ab5ca46ca0266e7e58627e682be70f71274d99
Bill Wilke <billwilke2@gmail.com>
2020-05-23 10:24:28 -0600

    Initial Commit

---
+++ src/userRepository.js
@@ -0,0 +1,11 @@
+class UserRepository {
+    constructor(userData, recipeData, pantryData) {
+      this.userData = userData;
+      this.recipeData = recipeData;
+      this.pantryData = pantryData;
+      this.cookbookData = cookbookData;
+      this.users = [];
+
+    }
+  }
+  export default UserRepository;
