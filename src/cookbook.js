0796ebcfa143ba56d4d6eb4273fd69e379cc5032
Bill Wilke <billwilke2@gmail.com>
2020-05-25 11:12:26 -0600

    Adjusts ingredientsbyid method

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,32 +1,58 @@
 class Cookbook {
-  constructor(data, recipeWithNames) {
+  constructor(data) {
     this.recipes = data;
-    this.recipesWithNames = recipeWithNames
   }
 
-  findRecipe(searchText) {
-    return this.recipes.filter(recipe => {
-      return recipe.ingredients.find(ingredient => {
-        return (ingredient.name.includes(searchText)) ||
-        (recipe.name.includes(searchText))
-      });
-    })
-  }
+  // findRecipe(searchText) {
+  //   return this.recipes.filter(recipe => {
+  //     return recipe.ingredients.find(ingredient => {
+  //       return (ingredient.name.includes(searchText)) ||
+  //       (recipe.name.includes(searchText))
+  //     });
+  //   })
+  // }
 
-  addNameToRecipeIngredients(ingredientsData) {
-    console.log('recipes', this.recipes)
-    const ingredientNames = this.recipes.map(recipe => {
+  // addNameToRecipeIngredients(ingredientsData) {
+  //   // const ingredientNames = this.recipes.map(recipe => {
+  //   //   recipe.ingredients.forEach(ingredient => {
+  //   //     ingredientsData.forEach(item => {
+  //   //      if(ingredient.id === item.id) {
+  //   //        ingredient['name'] = item.name
+  //   //      }
+  //   //    })
+  //   //   })  
+  //   //   return ingredient
+  //   // })
+  //   // console.log('ingredientNames', ingredientNames)
+  //   // return ingredientNames
+  //  }
+
+  allPossibleIngredients() {
+    let allIngredients = [];
+    this.recipes.forEach(recipe => {
       recipe.ingredients.forEach(ingredient => {
-        ingredientsData.forEach(item => {
-         if(ingredient.id === item.id) {
-           ingredient['name'] = item.name
-         }
-         return ingredient
-       })
-      })  
+        if(!allIngredients.includes(ingredient)) {
+          allIngredients.push(ingredient)
+        }
+      })
     })
+    console.log('allingredients', allIngredients)
+    return allIngredients
+  }
+
+  returnIngredientsWithNames(ingredientsData) {
+    const allPossibleIngredients = this.allPossibleIngredients()
+    const ingredientNames = allPossibleIngredients.map(ingredient => {
+      ingredientsData.find(item => {
+        if(ingredient.id === item.id) {
+          ingredient['name'] = item.name
+        }
+      })
+      return ingredient
+    })
+    console.log(ingredientNames)
     return ingredientNames
-   }
+  }
 }
 
 export default Cookbook;
