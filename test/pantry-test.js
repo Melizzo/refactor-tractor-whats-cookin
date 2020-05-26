dcb654f1e31351e1149900cabfed53b4f9aac62a
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-05-26 09:09:15 -0600

    :bar_chart: cleaning up unneeded pantry tests

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -1,32 +1,20 @@
 import { expect } from 'chai';
 
-import Recipe from '../src/recipe.js';
-import recipeData from '../src/data/recipes.js';
+import Pantry from '../src/pantry.js';
 import ingredientsData from '../src/data/ingredients.js';
 
-let recipe;
+let pantry;
 
-describe('Recipe', () => {
+describe('Pantry', () => {
   beforeEach(() => {
+    pantry = new Pantry(userIngredients);
 
-    recipe = new Recipe(recipeData[47], ingredientsData);
   });
 
-  describe('Recipe Data', () => {
 
-    it('Should hold its own ingredient data', () => {
-      expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
-    })
-
-    it('Should hold its own instruction data', () => {
-      expect(recipe.instructions).to.equal(recipeData[47].instructions);
-    })
+  it('Should hold its own ingredient data', () => {
+    expect(pantry.ingredients).to.equal(recipeData[47].ingredients);
   })
-
-  it('Should be able to calculate the cost of its ingredients', () => {
-    // console.log(ingredientsData);
-    expect(recipe.calculateCost()).to.equal(4166);
-  });
 
 
 });
