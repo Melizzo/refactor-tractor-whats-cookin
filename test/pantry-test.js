ecc6dd6afed236f027bf8ba1ec458c2e5dad108b
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-05-28 17:35:15 -0600

    :bar_chart: pantry method to combine arrays test

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -14,7 +14,7 @@ describe('Pantry', () => {
   beforeEach(() => {
     user = new User(1, 'Boba', [
       {
-        'ingredient': 1077,
+        'ingredient': 1041009,
         'amount': 1
       },
       {
@@ -26,17 +26,11 @@ describe('Pantry', () => {
         'amount': 3
       }]
     );
-      console.log('user', user);
       
     pantry = new Pantry(user.pantry);
-
-    console.log('the pantry', pantry);
-    
-    console.log('pantry', user.pantry);
-    
     recipe = new Recipe(recipeData[47], ingredientsData)
-
   });
+
   it('should be a function', function() {
     expect(Pantry).to.be.a('function')
   });
@@ -53,7 +47,86 @@ describe('Pantry', () => {
   })
 
   it('should be able to find missing ingredients', () => {
-    expect(pantry.findMissingIngredients(recipeData[47].ingredients)).to.equal()
+    expect(pantry.findMissingIngredients(recipeData[47].ingredients)).to.deep.equal([
+      {
+        name: 'flatbread',
+        id: 10018413,
+        quantity: { amount: 1, unit: '' }
+      },
+      {
+        name: 'fresh basil',
+        id: 2044,
+        quantity: { amount: 3, unit: 'leaves' }
+      },
+      {
+        name: 'grape tomatoes',
+        id: 10111529,
+        quantity: { amount: 0.5, unit: 'cup' }
+      },
+      {
+        name: 'olive oil',
+        id: 4053,
+        quantity: { amount: 1, unit: 'teaspoon' }
+      },
+      { name: 'zucchini', id: 11477, quantity: { amount: 1, unit: 'cup' } }  
+    ])
+  })
+
+  it('it should find the ingredients that the user doesn/t have enough of', () => {
+    expect(pantry.findIngredientsWeNeedMoreOf(recipeData[47].ingredients)).to.deep.equal([{
+      id: 1041009,
+      quantity: { amount: 1, unit: 'tablespoons' }
+    },])
+  })
+
+  it('it should be able to combine two arrays', () => {
+    expect(pantry.returnCombinedArrays(recipeData[47].ingredients)).to.deep.equal([{
+          "id" : 10018413,
+          "name": "flatbread",
+          "quantity": {
+            "amount": 1,
+            "unit": ""
+          }
+        },
+        {
+          "id": 2044,
+          "name": "fresh basil",
+          "quantity": {
+            "amount": 3,
+            "unit": "leaves"
+          }
+        },
+        {
+          "id": 10111529,
+          "name": "grape tomatoes",
+          "quantity": {
+            "amount": 0.5,
+            "unit": "cup"
+          }
+        },
+        {
+          "id": 4053,
+          "name": "olive oil",
+          "quantity": {
+            "amount": 1,
+            "unit": "teaspoon"
+          }
+        },
+        {
+          "id": 11477,
+          "name": "zucchini",
+          "quantity": {
+            "amount": 1,
+            "unit": "cup"
+          }
+        },
+        {
+          "id": 1041009,
+          "quantity": {
+            "amount": 1,
+            "unit": "tablespoons"
+          }
+        }])
   })
 
 
