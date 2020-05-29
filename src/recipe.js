5ee8a53b61957d11a3969e516ac47a5917f1b988
Jeremiah Black <jeremiahblackol@gmail.com>
2020-05-29 12:02:16 -0600

    user test for review

--- src/recipe.js
+++ src/recipe.js
@@ -1,28 +1,28 @@
 class Recipe {
   constructor(recipe, ingredientsData) {
     if(recipe instanceof Object, ingredientsData instanceof Array) {
-    this.name = recipe.name;
-    this.id = recipe.id;
-    this.ingredients = recipe.ingredients;
-    this.instructions = recipe.instructions;
-    this.tags = recipe.tags;
-    this.ingredientsData = ingredientsData;
-    this.isFavorite = false;
-    this.isRecipeToCook = false;
+      this.name = recipe.name;
+      this.id = recipe.id;
+      this.ingredients = recipe.ingredients;
+      this.instructions = recipe.instructions;
+      this.tags = recipe.tags;
+      this.ingredientsData = ingredientsData;
+      this.isFavorite = false;
+      this.isRecipeToCook = false;
     }
   }
 
   searchIngredientsById(ingredientsData) {
-  if(ingredientsData) {
-   const ingredientNames = this.ingredients.map(ingredient => {
-      ingredientsData.find(item => {
-        if(ingredient.id === item.id) {
-          ingredient['name'] = item.name
-        }
+    if(ingredientsData) {
+      const ingredientNames = this.ingredients.map(ingredient => {
+        ingredientsData.find(item => {
+          if(ingredient.id === item.id) {
+            ingredient['name'] = item.name
+          }
+        })
+        return ingredient
       })
-      return ingredient
-    })
-    return ingredientNames
+      return ingredientNames
     } 
   }
 
